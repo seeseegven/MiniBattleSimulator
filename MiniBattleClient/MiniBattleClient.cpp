f36b76fb4843ef4e58c199b66bbe641c29f7801b
yqggg <3264059609@qq.com>
2026-08-17 17:53:53 +0800

    服务端和客户端最小化完成，可以正常连接到服务器

---
+++ MiniBattleClient/MiniBattleClient.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	WSADATA wsaData{};
+	
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		std::cout << "WSAStartUp failed\n";
+		return 1;
+	}
+
+	SOCKET clientSocket = socket(
+		AF_INET,
+		SOCK_STREAM,
+		IPPROTO_TCP
+	);
+
+	if (clientSocket == INVALID_SOCKET) {
+		std::cout << "socket failed\n";
+		WSACleanup();
+		return 1;
+	}
+
+	sockaddr_in serverAddr{};
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(8888);
+
+	inet_pton(
+		AF_INET,
+		"127.0.0.1",
+		&serverAddr.sin_addr
+	);
+	std::cout << "Connecting to server...\n";
+	if (connect(clientSocket,
+		reinterpret_cast<sockaddr*>(&serverAddr),
+		sizeof(serverAddr))== SOCKET_ERROR) {
+		std::cout << "connect failed\n";
+		closesocket(clientSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	std::cout << "connected to server!\n";
+	closesocket(clientSocket);
+	WSACleanup();
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
---
+++ MiniBattleServer/MiniBattleServer.cpp
@@ -0,0 +1,70 @@
+﻿// MiniBattleServer.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+    //初始化winsock
+    WSADATA wsaData{};
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cout << "WSAStartUp failed" << std::endl;
+        return 1;
+    }
+
+    SOCKET listenSocket = socket(
+        AF_INET,//ipv4
+        SOCK_STREAM,//字节流
+        IPPROTO_TCP//tcp协议
+    );
+
+    if (listenSocket == INVALID_SOCKET) {
+        std::cout << "socket failed\n";
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8888);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(listenSocket, reinterpret_cast<sockaddr*>(&serverAddr),
+        sizeof(serverAddr)) == SOCKET_ERROR) {
+            std::cout << "bind failed\n";
+            closesocket(listenSocket);
+            WSACleanup();
+            return 1;
+    }
+
+    if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cout << "listen failed\n";
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server is watting for client...\n";
+
+    SOCKET clientSocket = accept(listenSocket, nullptr, nullptr);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cout << "accept failed\n";
+    }
+    else {
+        std::cout << "Client connected!\n";
+    }
+
+    if (clientSocket != INVALID_SOCKET) {
+        closesocket(clientSocket);
+    }
+    closesocket(listenSocket);
+    WSACleanup();
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
